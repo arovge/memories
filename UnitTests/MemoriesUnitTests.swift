6ca5fd9f9b6d1fbda957b5459437585c1d843c06
Austin Rovge <austinrovge@gmail.com>
2022-05-29 01:13:08 -0500

    add scheme for ui tests

--- UnitTests/MemoriesUnitTests.swift
+++ UnitTests/MemoriesUnitTests.swift
@@ -11,12 +11,12 @@ class memoriesTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func test_example() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
+    func test_performanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
